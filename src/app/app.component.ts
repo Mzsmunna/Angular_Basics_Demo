29bd298d2e3d8224a73565275cab81abc44225e3
Mzs Munna <mzs.munna@gmail.com>
2022-03-06 10:20:39 +0600

    added more html elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_Basics_Demo';
+
+  ngOnInit() {
+    //var carousel = document.querySelector('.carousel')! as HTMLCanvasElement
+    //carousel.carousel({
+    //  interval: 2000
+    //})
+  }
 }
