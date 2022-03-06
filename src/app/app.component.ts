57369197c857458a084ceabcbc81b8adb8fed8b4
Md. Mamunuz Zaman Sarker <mzaman@insightintechnology.com>
2022-03-06 13:07:11 +0600

    added @ng-bootstrap/ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class AppComponent {
-  title = 'Angular_Basics_Demo';
 
-  ngOnInit() {
-    //var carousel = document.querySelector('.carousel')! as HTMLCanvasElement
-    //carousel.carousel({
-    //  interval: 2000
-    //})
+  title: string = 'Angular_Basics_Demo';
+
+  images: {title: string, short: string, src: string}[] = [
+    {title: 'First Slide', short: 'First Slide Short', src: "https://picsum.photos/id/700/900/500"},
+    {title: 'Second Slide', short: 'Second Slide Short', src: "https://picsum.photos/id/1011/900/500"},
+    {title: 'Third Slide', short: 'Third Slide Short', src: "https://picsum.photos/id/984/900/500"}
+  ];
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 1000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
   }
+
+  // ngOnInit() {
+  //   var carouselConfig = document.querySelector('carousel')! as any
+  //   carouselConfig.carousel({
+  //     interval: 2000,
+  //     keyboard: true,
+  //     pauseOnHover: true
+  //   })
+  // }
 }
