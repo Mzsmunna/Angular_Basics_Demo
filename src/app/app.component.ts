b7e3302ab93512847c85b6aa532127d8e4c40a13
Md. Mamunuz Zaman Sarker <mzaman@insightintechnology.com>
2022-03-06 22:41:18 +0600

    Lesson : Angular LifeCycle Hooks / Methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,14 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  // <h2 class="display-2">I am Mzs.</h2>
+  // <h3 class="display-3">Hello World!</h3>
+  // `,
   styleUrls: ['./app.component.css'],
   providers: [NgbCarouselConfig]
 })
-export class AppComponent { //implements OnChanges,  OnInit,  DoCheck,  AfterContentInit,  AfterContentChecked,  AfterViewInit,  AfterViewChecked,  OnDestroy
+export class AppComponent { //implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
 
   title: string = 'Welcome to Basics of Angular';
   count: number = 0;
@@ -36,18 +40,18 @@ export class AppComponent { //implements OnChanges,  OnInit,  DoCheck,  AfterCon
   // }
 
 //#region Angular LifeCycle Hooks / Methods
-  ngOnChanges() {
-    console.log(`ngOnChanges: Invoked every time there is a change in one of th input properties of the component.
-    For example => @Input() decorator`);
-  }
-
   ngOnInit() {
     console.log(`ngOnInit: Invoked when given component has been initialized.
     This hook is only called once after the first ngOnChanges`);
   }
 
+  ngOnChanges() {
+    console.log(`ngOnChanges: Invoked every time there is a change in one of th input properties of the component.
+    For example => @Input() decorator`);
+  }
+
   ngDoCheck() {
-    console.log(`ngDoCheck: Invoked when the change detector of the given component is invoked.
+    console.log(`ngDoCheck: Invoked when the change detector of the given component is invoked => Developer’s custom change detection.
     It allows us to implement our own change detection algorithm for the given component.
 
     Important => ngDoCheck and ngOnChanges should not be implemented together on the same component.`)
@@ -60,22 +64,26 @@ export class AppComponent { //implements OnChanges,  OnInit,  DoCheck,  AfterCon
 
   //Note: These hooks are only called for components and not directives.(Hooks for the Component’s Children)
   ngAfterContentInit() {
-    console.log("ngAfterContentInit: Invoked after Angular performs any content projection into the component’s view");
+    console.log(`ngAfterContentInit: Invoked after Angular performs any content projection into the component’s view.
+    Called when the component’s content ngContent is initialized`);
   }
 
   ngAfterContentChecked() {
-    console.log("ngAfterContentChecked: Invoked each time the content of the given component has been checked by the change detection mechanism of Angular.");
+    console.log(`ngAfterContentChecked: Invoked each time the content of the given component has been checked by the change detection mechanism of Angular.
+    Called when the component’s content is updated or checked for updates`);
   }
 
   ngAfterViewInit() {
-    console.log("ngAfterViewInit: Invoked when the component’s view has been fully initialized.");
+    console.log(`ngAfterViewInit: Invoked when the component’s view has been fully initialized.
+    Called when the component’s projected view has been initialized`);
   }
 
   ngAfterViewChecked() {
-    console.log("ngAfterViewChecked: Invoked each time the view of the given component has been checked by the change detection mechanism of Angular.");
+    console.log(`ngAfterViewChecked: Invoked each time the view of the given component has been checked by the change detection mechanism of Angular.
+    Called after the projected view has been checked`);
   }
 
-  readonly lifeCycleHooksRef: string = "https://www.freecodecamp.org/news/angular-lifecycle-hooks/";
+  readonly lifeCycleHooksRefs: string[] = ["https://www.freecodecamp.org/news/angular-lifecycle-hooks/", "https://blog.logrocket.com/angular-lifecycle-hooks/", "https://codecraft.tv/courses/angular/components/lifecycle-hooks/"];
   //#endregion
 
   Hide(): void {
