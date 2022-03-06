b833775a982179cb396f14f5f4cc99d1259a484b
Md. Mamunuz Zaman Sarker <mzaman@insightintechnology.com>
2022-03-06 22:20:41 +0600

    Lesson : Angular LifeCycle Hooks / Methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent { //implements OnChanges,  OnInit,  DoCheck,  AfterCon
     config.keyboard = true;
     config.pauseOnHover = true;
 
-    console.log("This is invoked when Angular creates a component or directive by calling new on the class.");
+    console.log("constructor: This is invoked when Angular creates a component or directive by calling new on the class.");
   }
 
   // ngOnInit() {
@@ -37,41 +37,41 @@ export class AppComponent { //implements OnChanges,  OnInit,  DoCheck,  AfterCon
 
 //#region Angular LifeCycle Hooks / Methods
   ngOnChanges() {
-    console.log("Invoked every time there is a change in one of th input properties of the component.");
+    console.log("ngOnChanges: Invoked every time there is a change in one of th input properties of the component.");
   }
 
   ngOnInit() {
-    console.log(`Invoked when given component has been initialized.
+    console.log(`ngOnInit: Invoked when given component has been initialized.
     This hook is only called once after the first ngOnChanges`);
   }
 
   ngDoCheck() {
-    console.log(`Invoked when the change detector of the given component is invoked.
+    console.log(`ngDoCheck: Invoked when the change detector of the given component is invoked.
     It allows us to implement our own change detection algorithm for the given component.
 
-    Important : ngDoCheck and ngOnChanges should not be implemented together on the same component.`)
+    Important => ngDoCheck and ngOnChanges should not be implemented together on the same component.`)
   }
 
   ngOnDestroy() {
-    console.log(`This method will be invoked just before Angular destroys the component.
+    console.log(`ngOnDestroy: This method will be invoked just before Angular destroys the component.
     Use this hook to unsubscribe observables and detach event handlers to avoid memory leaks.`);
   }
 
   //Note: These hooks are only called for components and not directives.(Hooks for the Component’s Children)
   ngAfterContentInit() {
-    console.log("Invoked after Angular performs any content projection into the component’s view");
+    console.log("ngAfterContentInit: Invoked after Angular performs any content projection into the component’s view");
   }
 
   ngAfterContentChecked() {
-    console.log("Invoked each time the content of the given component has been checked by the change detection mechanism of Angular.");
+    console.log("ngAfterContentChecked: Invoked each time the content of the given component has been checked by the change detection mechanism of Angular.");
   }
 
   ngAfterViewInit() {
-    console.log("Invoked when the component’s view has been fully initialized.");
+    console.log("ngAfterViewInit: Invoked when the component’s view has been fully initialized.");
   }
 
   ngAfterViewChecked() {
-    console.log("Invoked each time the view of the given component has been checked by the change detection mechanism of Angular.");
+    console.log("ngAfterViewChecked: Invoked each time the view of the given component has been checked by the change detection mechanism of Angular.");
   }
 
   readonly lifeCycleHooksRef: string = "https://www.freecodecamp.org/news/angular-lifecycle-hooks/";
